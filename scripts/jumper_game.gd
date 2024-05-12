33a5820ba743bac0e64e9ef43dd2b39d12d86aa2
June-Han <julialiu08@gmail.com>
2024-05-12 19:43:02 +0800

    Respawn system working properly

--- scripts/jumper_game.gd
+++ scripts/jumper_game.gd
@@ -17,4 +17,8 @@ func _on_killed_enemy(enemy_name, enemy_pos):
 	var enemy = load("res://scenes/"+ str(enemy_name) + ".tscn")
 	var new_enemy = enemy.instantiate()
 	new_enemy.global_position = enemy_pos
+	#Signals within the enemy object itself will be instantiated. 
+	#But the signal from the instantiated object received by functions 
+	#outside of object need to be connected by code outside of the object before adding it as child
+	new_enemy.killed_enemy.connect(_on_killed_enemy) #Connecting the killed_enemy signal within the object to this function (Only this signal is not instantiated)
 	add_child(new_enemy)
